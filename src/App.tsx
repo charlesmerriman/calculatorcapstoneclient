aeda858c6243024403c85786aa910df7cf870877
charlesmerriman <zacmerr@gmail.com>
2025-12-17 15:46:07 -0600

    Created initial login/register files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { ApplicationViews } from './views/ApplicationViews.js'
+import { Login } from './components/auth/Login.js'
+import { Register } from './components/auth/Register.js'
 
 function App() {
   return (
     <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       <Route path='*' element={<ApplicationViews />}/>
     </Routes>
   )
@@ -12,5 +16,3 @@ function App() {
 
 export default App
 
-//      <Route path='/login' element={} />
-//      <Route path='/register' element={} />
---
+++ src/components/auth/Register.tsx
@@ -0,0 +1,91 @@
+export const Register = () => {
+  return (
+    <>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-100">
+                  First Name
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="first-name"
+                  required
+                  autoComplete="first-name"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-100">
+                  Last Name
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="last-name"
+                  required
+                  autoComplete="last-name"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
